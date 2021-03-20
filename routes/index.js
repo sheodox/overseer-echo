c544ad805eb2c2e0937048ec6ca87d52128cfecc
sheodox <3468630+sheodox@users.noreply.github.com>
2021-03-20 10:20:10 -0500

    Much needed overhaul!
    This is being done in combination with an Overseer rewrite. As far as Echo is concerned a lot of the process has been redone. Echo now must provide an integration token generated in Overseer otherwise Overseer will refuse its websocket connection. Uploads now use an ID provided by Overseer for the file names, so user's original file names are no longer used at all. This lets us just validate that we're using a UUID for the file path instead of having to check isPathInside for security.
    
    Downloads now will only be served to clients that provide an integration token. A message is sent to Overseer asking it to validate the token gives them permission to download the file before serving it.
    
    Uploads will only be accepted if they provide an ID Echo is expecting (which is given to Echo by Overseer before the upload starts).
    
    Items in Overseer are not implicitly created because they exist in Echo, so when emitting a 'refresh' to overseer we now only provide the disk usage stats.
    
    This app was originally generated using the express generator so it had a lot of dependencies that were never used. I've removed them! The important dependencies have been updated as well.
    
    Configuration is now done through a .env file instead of a config.json file.

--- routes/index.js
+++ routes/index.js
@@ -1,87 +1,102 @@
-var router = require('express').Router(),
+/**
+ * Upload series of events:
+ * 1. User submits item metadata on Overseer which is inserted to the DB and an ID is generated
+ * 2. Overseer sends the ID to Echo to know to accept an upload with for it
+ * 3. User posts a file upload to Echo with the agreed upon ID (without that ID the upload is rejected)
+ * 4. Once the file is fully uploaded Echo sends a message to Overseer
+ */
+
+const router = require('express').Router(),
     exec = require('child_process').exec,
     fs = require('fs'),
     path = require('path'),
     Busboy = require('busboy'),
-    isPathInside = require('is-path-inside'),
-    config = require('../config'),
-    socket = require('socket.io-client').connect(config.overseer + '/echo-server'),
-    storageDir = config.storagePath || './storage';
-
-socket.on('connect', sendList);
-function sendList() {
-    //get list of available games
-    fs.readdir(storageDir, function(err, files) {
-        if (err) {
-            files = [];
+    socket = require('socket.io-client')(`${process.env.OVERSEER_HOST}/echo-server`, {
+        auth: {
+            token: process.env.OVERSEER_TOKEN
         }
+    }),
+    //Keep track of all of the IDs we have stored, so we can 404 without having to stat the file
+    storedItemIds = new Set(),
+    //For echo to accept an upload it needs to be made aware of an ID incoming from Overseer,
+    //without an expected ID it will reject the upload. The ID is used as the file name here.
+    expectedUploadIds = new Set(),
+    storageDir = process.env.STORAGE_PATH || './storage',
+    uuid = require('uuid');
 
-        Promise.all([getUsedDisk(), statAll(files)])
-            .then(([diskUsage, gamesList]) => {
-                socket.emit('refresh', {
-                    games: gamesList,
-                    diskUsage
-                });
-            })
-            .catch(err => console.log(err));
-    });
-}
-socket.on('delete', function(game) {
-    var gamepath = path.join(storageDir, game + '.zip');
-    if (isPathInside(gamepath, storageDir)) {
-        console.log(`deleting ${gamepath}`);
-        fs.unlink(gamepath, err => {
-            if (err) {
-                console.log(err);
-                socket.emit('error', err);
-            }
-            else {
-                getUsedDisk()
-                    .then(diskUsage => {
-                        socket.emit('delete-game', {
-                            diskUsage,
-                            file: game
-                        })
-                    });
-            }
-        });
-    }
-    else {
-        socket.emit('error', 'invalid game path');
+socket.on('connect', () => {
+    console.log(`Connected to Overseer at ${new Date().toLocaleString()}`);
+    refreshOverseer();
+});
+
+socket.on('expect-upload', (id, ack) => {
+    console.log(`Told to expect an upload for ${id}`);
+    //this eventually turns into a file name, ensure we only use predictable safe file paths by
+    //only allowing UUIDs (though we should theoretically never get anything weird because it
+    //comes from overseer itself, just in case!)
+    if (uuid.validate(id)) {
+        expectedUploadIds.add(id);
+        //need to tell Overseer we're ready to accept the upload so the browser will post it
+        ack();
     }
 });
 
-router.post('/upload', function(req, res) {
-    //forward games on to the backup server
-    let busboy = Busboy({headers: req.headers}),
-        fields = {
-            in_progress: true,
-            size: 0,
-            modified: new Date().toISOString()
-        },
-        gameName;
+socket.on('delete', (id, done) => {
+    const itemPath = path.join(storageDir, id + '.zip');
+
+    //if for whatever reason we don't actually have this ID stored just say we've done it
+    if (!storedItemIds.has(id)) {
+        done();
+    }
+
+    if (uuid.validate(id)) {
+        console.log(`deleting ${id}`);
+        fs.unlink(itemPath, err => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                done();
+                refreshOverseer();
+            }
+        });
+    }
+});
+
+async function refreshOverseer() {
+    socket.emit('refresh', {
+        diskUsage: await getUsedDisk()
+    });
+}
+
+router.post('/upload/:id', function(req, res) {
+    const busboy = Busboy({headers: req.headers}),
+        id = req.params.id;
+
+    if (!expectedUploadIds.has(id)) {
+        res.status(412); //412 = Precondition Failed
+        res.send();
+        return;
+    }
+    expectedUploadIds.delete(id);
 
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
         console.log(`uploading ${filename}`);
-        gameName = filename;
-        const stream = new fs.createWriteStream(path.join(storageDir, filename));
+        const stream = new fs.createWriteStream(path.join(storageDir, `${id}.zip`));
+
         file.pipe(stream)
             .on('error', err => {
                 console.log(err);
             });
 
-        file.on('end', function() {
-            Promise.all([getUsedDisk(), statGame(gameName)()])
-                .then(([diskUsage, gameData]) => {
-                    socket.emit('new-game', {
-                        diskUsage,
-                        game: Object.assign(fields, gameData, {
-                            in_progress: false
-                        })
-                    });
-                    console.log(gameName, gameData);
-                });
-            res.json({done: true});
+        file.on('end', async () => {
+            const size = await statUpload(id);
+            socket.emit('uploaded', id, {
+                size
+            });
+
+            storedItemIds.add(id);
+            res.send();
         });
     });
 
@@ -92,10 +107,33 @@ router.post('/upload', function(req, res) {
     req.pipe(busboy);
 });
 
-router.get('/download/:game', function(req, res) {
-    console.log(req.params.game);
-    socket.emit('downloaded', req.params.game.replace('.zip', ''));
-    fs.createReadStream(path.join(storageDir, req.params.game)).pipe(res);
+router.get('/download/:id', function(req, res) {
+    const id = req.params.id,
+        token = req.query.token;
+
+    // before trying anything, make sure there's actually something here.
+    // this should probably never happen, but just extra precaution!
+    if (!storedItemIds.has(id)) {
+        res.status(404);
+        res.send(`Error 404: Nothing stored with that identifier.`)
+        return;
+    }
+
+    //Downloads will only be served to users who present a download token, a JWT generated
+    //by Overseer, we need to verify the token with Overseer before serving the file.
+    //Additionally Overseer will also give us the name of the item so the file can be
+    //downloaded with a file name that's not just an ugly UUID.
+    socket.emit('verify-download-token', token, id, ({allowed, name}) => {
+        if (allowed) {
+            socket.emit('downloaded', id);
+            res.set('Content-Disposition',  `attachment; filename="${name}.zip"`)
+            fs.createReadStream(path.join(storageDir, `${id}.zip`)).pipe(res);
+        }
+        else {
+            res.status(401)
+            res.send(`Error 401: You don't have permission to download that!`);
+        }
+    })
 });
 
 function getUsedDisk() {
@@ -106,7 +144,7 @@ function getUsedDisk() {
 
     return new Promise((resolve, reject) => {
         //get disk usage for the device the echo storage is on
-        exec(`df -B1 ${config.storagePath}`, (err, stdout, stderr) => {
+        exec(`df -B1 ${storageDir}`, (err, stdout, stderr) => {
             if (err) {
                 console.log(stderr);
                 reject(err);
@@ -124,39 +162,17 @@ function getUsedDisk() {
     });
 }
 
-function statAll(files) {
-    var p = Promise.resolve(),
-        games = [];
-
-    for (var i = 0; i < files.length; i++) {
-        p = p
-            .then(statGame(files[i]))
-            .then(stats => {
-                games.push(stats);
-            });
-    }
-
-    return p.then(() => {
-        return games});
-}
-
-function statGame(game) {
-    return () => {
-        return new Promise((resolve, reject) => {
-            fs.stat(path.join(storageDir,  game), (err, stats) => {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    resolve({
-                        file: game.replace('.zip', ''),
-                        size: stats.size,
-                        modified: stats.mtime
-                    })
-                }
-            })
+function statUpload(uploadId) {
+    return new Promise((resolve, reject) => {
+        fs.stat(path.join(storageDir,  `${uploadId}.zip`), (err, stats) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(stats.size);
+            }
         })
-    }
+    })
 }
 
 //ensure there's a folder for games to be saved
@@ -166,5 +182,12 @@ try {
 catch(e) {
     fs.mkdirSync(storageDir);
 }
+
+fs.readdirSync(storageDir).forEach(file => {
+    const id = path.basename(file, '.zip');
+    if (uuid.validate(id)) {
+        storedItemIds.add(id);
+    }
+})
 
 module.exports = router;
