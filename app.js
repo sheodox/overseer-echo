c0d6be8bbd44789ca699a22d753109928782610c
sheodox <3468630+sheodox@users.noreply.github.com>
2021-03-25 15:35:35 -0500

    If some some environment variables (OVERSEER_HTTPS_KEY_PATH/OVERSEER_HTTPS_CERT_PATH) are defined, Echo will now boot as an HTTPS server

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 require('dotenv').config();
-const express = require('express'),
+const {OVERSEER_HTTPS_KEY_PATH, OVERSEER_HTTPS_CERT_PATH} = process.env,
+    http = !!OVERSEER_HTTPS_CERT_PATH ? require('https') : require('http'),
+    express = require('express'),
+    fs = require('fs'),
     routes = require('./routes/index'),
     logger = require('morgan'),
     cors = require("cors"),
     app = express();
+
+let server;
+if (!!OVERSEER_HTTPS_CERT_PATH) {
+    server = http.createServer({
+        key: fs.readFileSync(OVERSEER_HTTPS_KEY_PATH),
+        cert: fs.readFileSync(OVERSEER_HTTPS_CERT_PATH)
+    }, app);
+}
+else {
+    server = http.createServer(app);
+}
+
 
 app.use(logger('dev'));
 
@@ -13,6 +28,6 @@ app.use(cors({
 
 app.use('/', routes);
 
-app.listen(3000, function() {
+server.listen(3000, function() {
     console.log('listening on port 3000');
 });
